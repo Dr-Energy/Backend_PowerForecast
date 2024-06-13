a1cbc112c6180fd2503fa0685ad196267eb4618b
LeeYeongin <youngin5757@gmail.com>
2024-06-13 13:08:56 +0900

    0613 weatherService, Controller 수정

--- PowerForecast/src/main/java/edu/pnu/controller/WeatherController.java
+++ PowerForecast/src/main/java/edu/pnu/controller/WeatherController.java
@@ -1,21 +1,12 @@
 package edu.pnu.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClientException;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-
-import edu.pnu.DTO.AlertDTO;
 import edu.pnu.service.WeatherService;
 
 @RestController
@@ -26,19 +17,19 @@ public class WeatherController {
 	@GetMapping("/main/weather")
 	public ResponseEntity<?> getWeather(@RequestParam String sido,
             			   @RequestParam String gugun,
-            			   @RequestParam String eupmyeondong) throws UnsupportedEncodingException, URISyntaxException, JsonMappingException, JsonProcessingException {
+            			   @RequestParam String eupmyeondong) throws Exception {
 		try {
-			return weatherService.getWeatherData(sido, gugun, eupmyeondong);            
-        } catch (RestClientException e) {
+			return ResponseEntity.ok(weatherService.getWeatherData(sido, gugun, eupmyeondong));            
+        } catch (Exception e) {
         	return ResponseEntity.badRequest().body(e.getMessage());
-        }
+        } 
 	}
 	
 	@GetMapping("/main/weather/{regionId}")
-	public ResponseEntity<?> getWeather(@PathVariable Long regionId) throws JsonMappingException, JsonProcessingException, UnsupportedEncodingException, URISyntaxException{
+	public ResponseEntity<?> getWeather(@PathVariable Long regionId) throws Exception{
 		try {
-			return weatherService.getWeatherData(regionId);            
-        } catch (RestClientException e) {
+			return ResponseEntity.ok(weatherService.getWeatherData(regionId));            
+        } catch (Exception e) {
         	return ResponseEntity.badRequest().body(e.getMessage());
         }
 	}
--- PowerForecast/src/main/java/edu/pnu/service/WeatherService.java
+++ PowerForecast/src/main/java/edu/pnu/service/WeatherService.java
@@ -8,23 +8,19 @@ import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -51,62 +47,25 @@ public class WeatherService {
 	private String lon;
 	private String lat;
 
-	public ResponseEntity<?> getWeatherData(String sido, String gugun, String eupmyeondong)
-			throws UnsupportedEncodingException, URISyntaxException, JsonMappingException, JsonProcessingException {
+	public List<WeatherDTO> getWeatherData(String sido, String gugun, String eupmyeondong) throws Exception {
 		Region regionInfo = regionRepository.findBySidoAndGugunAndEupmyeondong(sido, gugun, eupmyeondong).get();
-		nx = regionInfo.getGridX();
-		ny = regionInfo.getGridY();
-		lon = regionInfo.getLongitude();
-		lat = regionInfo.getLatitude();
-
-		String[] dates = getDates();
-        String curDate = dates[0];
-        String baseDate = dates[1];
-
-		String baseTime = "2300";
-		String curTime = getCurrentTimeAdjusted();
-
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(new MediaType("application", "JSON", Charset.forName("UTF-8")));
-		URI uri = createAWSURI(baseDate, baseTime);
-		URI uri2 = createWeatherURI(curDate, curTime);
-		System.out.println(uri2);
-		
-		try {
-			ResponseEntity<String> response1 = restTemplate.getForEntity(uri, String.class);
-			ResponseEntity<String> response2 = restTemplate.getForEntity(uri2, String.class);
-			
-			String jsonRes1 = response1.getBody();
-			String jsonRes2 = response2.getBody();
-			
-			// ObjectMapper로 JSON 문자열을 객체로 받아오기
-			ObjectMapper objectMapper = new ObjectMapper();
-			JsonNode root = objectMapper.readTree(jsonRes1);
-		    JsonNode itemsNode = root.path("response").path("body").path("items").path("item");
-		    List<SkyItemDTO> items = objectMapper.readValue(itemsNode.toString(), new TypeReference<List<SkyItemDTO>>() {});
-
-			WeatherDTO weather = filterWeatherData(items, curDate, curTime);
-			weather = parseWeatherData(jsonRes2, weather);
-			return ResponseEntity.ok(weather);
-		} catch (HttpClientErrorException e) {
-			return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
-		} catch (RestClientException e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-		}
+		return getAPIData(regionInfo);
 	}
-	
-	public ResponseEntity<?> getWeatherData(Long regionId)
-			throws UnsupportedEncodingException, URISyntaxException, JsonMappingException, JsonProcessingException {
+
+	public List<WeatherDTO> getWeatherData(Long regionId) throws Exception {
 		Region regionInfo = regionRepository.findById(regionId).get();
+		return getAPIData(regionInfo);
+	}
+
+	private List<WeatherDTO> getAPIData(Region regionInfo) throws Exception {
 		nx = regionInfo.getGridX();
 		ny = regionInfo.getGridY();
 		lon = regionInfo.getLongitude();
 		lat = regionInfo.getLatitude();
 
 		String[] dates = getDates();
-        String curDate = dates[0];
-        String baseDate = dates[1];
+		String curDate = dates[0];
+		String baseDate = dates[1];
 
 		String baseTime = "2300";
 		String curTime = getCurrentTimeAdjusted();
@@ -115,33 +74,28 @@ public class WeatherService {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(new MediaType("application", "JSON", Charset.forName("UTF-8")));
 		URI uri = createAWSURI(baseDate, baseTime);
-		URI uri2 = createWeatherURI(curDate, curTime);
-		System.out.println(uri2);
-		
-		try {
-			ResponseEntity<String> response1 = restTemplate.getForEntity(uri, String.class);
-			ResponseEntity<String> response2 = restTemplate.getForEntity(uri2, String.class);
-			
-			String jsonRes1 = response1.getBody();
-			String jsonRes2 = response2.getBody();
-			
-			// ObjectMapper로 JSON 문자열을 객체로 받아오기
-			ObjectMapper objectMapper = new ObjectMapper();
-			JsonNode root = objectMapper.readTree(jsonRes1);
-		    JsonNode itemsNode = root.path("response").path("body").path("items").path("item");
-		    List<SkyItemDTO> items = objectMapper.readValue(itemsNode.toString(), new TypeReference<List<SkyItemDTO>>() {});
+		URI uri2 = createWeatherURI(baseDate, curDate, curTime);
 
-			WeatherDTO weather = filterWeatherData(items, curDate, curTime);
-			weather = parseWeatherData(jsonRes2, weather);
-			return ResponseEntity.ok(weather);
-		} catch (HttpClientErrorException e) {
-			return ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
-		} catch (RestClientException e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-		}
+		ResponseEntity<String> response1 = restTemplate.getForEntity(uri, String.class);
+		ResponseEntity<String> response2 = restTemplate.getForEntity(uri2, String.class);
+
+		String jsonRes1 = response1.getBody();
+		String jsonRes2 = response2.getBody();
+
+		// ObjectMapper로 JSON 문자열을 객체로 받아오기
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode root = objectMapper.readTree(jsonRes1);
+		JsonNode itemsNode = root.path("response").path("body").path("items").path("item");
+		List<SkyItemDTO> items = objectMapper.readValue(itemsNode.toString(), new TypeReference<List<SkyItemDTO>>() {
+		});
+
+		WeatherDTO weather = filterWeatherData(items, curDate, curTime);
+		List<WeatherDTO> listWeather = parseWeatherData(jsonRes2, weather);
+		return listWeather;
+
 	}
 
-	public URI createAWSURI(String baseDate, String baseTime) throws UnsupportedEncodingException, URISyntaxException {
+	private URI createAWSURI(String baseDate, String baseTime) throws UnsupportedEncodingException, URISyntaxException {
 		String url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst";
 		String serviceKey = decode_api_key;
 		String encodedServiceKey;
@@ -157,17 +111,19 @@ public class WeatherService {
 		builder.append("&" + URLEncoder.encode("ny", "UTF-8") + "=" + URLEncoder.encode(ny, "UTF-8"));
 		return new URI(builder.toString());
 	}
-	
-	public URI createWeatherURI(String curDate, String curTime) throws UnsupportedEncodingException, URISyntaxException {
+
+	public URI createWeatherURI(String baseDate, String curDate, String curTime)
+			throws UnsupportedEncodingException, URISyntaxException {
 		String url = "https://apihub.kma.go.kr/api/typ01/url/sfc_nc_var.php";
 		String serviceKey = weather_api_key;
 		String encodedServiceKey;
 		encodedServiceKey = URLEncoder.encode(serviceKey, "UTF-8");
 		StringBuilder builder = new StringBuilder(url);
-		builder.append("?" + URLEncoder.encode("tm1", "UTF-8") + "=" + URLEncoder.encode(curDate+curTime, "UTF-8"));
-		builder.append("&" + URLEncoder.encode("tm2", "UTF-8") + "=" + URLEncoder.encode(curDate+curTime, "UTF-8"));
-		builder.append("&" + URLEncoder.encode("obs", "UTF-8") + "=" + URLEncoder.encode("ta,hm,rn_60m,ta_chi", "UTF-8"));
-		builder.append("&" + URLEncoder.encode("itv", "UTF-8") + "=" + URLEncoder.encode("60", "UTF-8"));
+		builder.append("?" + URLEncoder.encode("tm1", "UTF-8") + "=" + URLEncoder.encode(baseDate + curTime, "UTF-8"));
+		builder.append("&" + URLEncoder.encode("tm2", "UTF-8") + "=" + URLEncoder.encode(curDate + curTime, "UTF-8"));
+		builder.append(
+				"&" + URLEncoder.encode("obs", "UTF-8") + "=" + URLEncoder.encode("ta,hm,rn_60m,ta_chi", "UTF-8"));
+		builder.append("&" + URLEncoder.encode("itv", "UTF-8") + "=" + URLEncoder.encode("1440", "UTF-8"));
 		builder.append("&" + URLEncoder.encode("lon", "UTF-8") + "=" + URLEncoder.encode(lon, "UTF-8"));
 		builder.append("&" + URLEncoder.encode("lat", "UTF-8") + "=" + URLEncoder.encode(lat, "UTF-8"));
 		builder.append("&" + URLEncoder.encode("help", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
@@ -176,15 +132,15 @@ public class WeatherService {
 	}
 
 	public static String[] getDates() {
-        LocalDate now = LocalDate.now();
-        LocalDate previousDay = now.minusDays(1);
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-        
-        String currentDate = now.format(dateFormatter);
-        String previousDate = previousDay.format(dateFormatter);
-        
-        return new String[] { currentDate, previousDate };
-    }
+		LocalDate now = LocalDate.now();
+		LocalDate previousDay = now.minusDays(1);
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+		String currentDate = now.format(dateFormatter);
+		String previousDate = previousDay.format(dateFormatter);
+
+		return new String[] { currentDate, previousDate };
+	}
 
 	public String getCurrentTimeAdjusted() {
 		// 현재 시간을 "HHmm" 형식의 문자열로 포맷
@@ -196,48 +152,48 @@ public class WeatherService {
 
 		return adjustedTime;
 	}
-	
+
 	private WeatherDTO filterWeatherData(List<SkyItemDTO> items, String date, String time) {
 		HashMap<String, String> filterData = new HashMap<>();
 		WeatherDTO weather;
-		
-        for (SkyItemDTO item : items) {
-            if (item.getFcstDate().equals(date) 
-            	&& item.getFcstTime().equals(time) 
-            	&& (item.getCategory().equals("SKY") || item.getCategory().equals("PTY"))) {
-                filterData.put(item.getCategory(), item.getFcstValue());
-            }
-        }
 
-        if(filterData.get("PTY").equals("0")) {
-        	weather = WeatherDTO.builder()
-        			.category("SKY")
-        			.value(filterData.get("SKY"))
-        			.build();
-        } else {
-        	weather = WeatherDTO.builder()
-        			.category("PTY")
-        			.value(filterData.get("PTY"))
-        			.build();
-        }
-        
-        return weather;
-    }
-	
-	public static WeatherDTO parseWeatherData(String response, WeatherDTO weatherDTO) {
-        // 응답에서 데이터 부분만 추출
-        String dataSection = response.split("# tm, ta, hm, rn_60m, ta_chi")[1].trim();
-        String[] lines = dataSection.split("\n");
-        // 데이터 라인을 쉼표(,)로 분리
-        String[] dataValues = lines[0].split(", ");
-        
-        
-        // 데이터 값을 DTO에 설정
-        weatherDTO.setTemp(dataValues[1]);
-        weatherDTO.setHumidity(dataValues[2]);
-        weatherDTO.setRain(dataValues[3]);
-        weatherDTO.setBodyTemp(dataValues[4]);
+		for (SkyItemDTO item : items) {
+			if (item.getFcstDate().equals(date) && item.getFcstTime().equals(time)
+					&& (item.getCategory().equals("SKY") || item.getCategory().equals("PTY"))) {
+				filterData.put(item.getCategory(), item.getFcstValue());
+			}
+		}
 
-        return weatherDTO;
-    }
+		if (filterData.get("PTY").equals("0")) {
+			weather = WeatherDTO.builder().category("SKY").value(filterData.get("SKY")).build();
+		} else {
+			weather = WeatherDTO.builder().category("PTY").value(filterData.get("PTY")).build();
+		}
+
+		return weather;
+	}
+
+	public static List<WeatherDTO> parseWeatherData(String response, WeatherDTO curWeatherDTO) {
+		// 응답에서 데이터 부분만 추출
+		String dataSection = response.split("# tm, ta, hm, rn_60m, ta_chi")[1].trim();
+		String[] lines = dataSection.split("\n");
+
+		// 데이터 라인을 쉼표(,)로 분리
+		String[] dataValues1 = lines[0].split(", ");
+		String[] dataValues2 = lines[1].split(", ");
+
+		WeatherDTO prevWeatherDTO = WeatherDTO.builder().temp(dataValues1[1]).humidity(dataValues1[2])
+				.rain(dataValues1[3]).bodyTemp(dataValues1[4]).build();
+
+		// 데이터 값을 DTO에 설정
+		curWeatherDTO.setTemp(dataValues2[1]);
+		curWeatherDTO.setHumidity(dataValues2[2]);
+		curWeatherDTO.setRain(dataValues2[3]);
+		curWeatherDTO.setBodyTemp(dataValues2[4]);
+
+		List<WeatherDTO> result = new ArrayList<>();
+		result.add(prevWeatherDTO);
+		result.add(curWeatherDTO);
+		return result;
+	}
 }
