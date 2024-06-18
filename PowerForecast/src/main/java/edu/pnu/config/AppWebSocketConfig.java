104a642e110a97616c70ea6cca7d79384c90d7c7
LeeYeongin <youngin5757@gmail.com>
2024-06-18 15:17:53 +0900

    0618 클라이언트에게 전송할 메시지 수정

--- PowerForecast/src/main/java/edu/pnu/config/AppWebSocketConfig.java
+++ PowerForecast/src/main/java/edu/pnu/config/AppWebSocketConfig.java
@@ -42,6 +42,7 @@ public class AppWebSocketConfig extends TextWebSocketHandler implements WebSocke
 		registry.addHandler(this, "pushservice").setAllowedOrigins("*");
 	}
 
+	// Client가 접속 시 호출되는 메서드
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         String query = session.getUri().getQuery();
@@ -68,32 +69,15 @@ public class AppWebSocketConfig extends TextWebSocketHandler implements WebSocke
         session.close(CloseStatus.NOT_ACCEPTABLE);
     }
 
+    // Client가 접속 해제 시 호출되는 메서드
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         System.out.println(session + " 클라이언트 접속 해제");
         clients.remove(session);
         sessionToRegionMap.remove(session);
     }			
-
-//	// Client가 접속 시 호출되는 메서드
-//		@Override
-//		public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//			clients.add(session);
-//		    System.out.println(session + " 클라이언트 접속");
-//		}
-//		
-//		// Client가 접속 해제 시 호출되는 메서드
-//		@Override
-//		public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-//			System.out.println(session + " 클라이언트 접속 해제");
-//			clients.remove(session);
-//		}		
-	// Client에서 메시지가 왔을 때 호출되는 메서드 ==> 채팅과 같은 형태의 기능을 추가하지 않는다면 필요없는 메소드이다.
-	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		System.out.println("Message : " + message.getPayload());
-	}
 	
+	// FE에게 정보를 푸시하는 메소드
 	public void sendPushMessage(Long regionId, String message) {
         if (clients.size() == 0) {
             System.out.println("No clients connected");
@@ -115,33 +99,4 @@ public class AppWebSocketConfig extends TextWebSocketHandler implements WebSocke
             }
         }
     }
-	// FE에게 정보를 푸시하는 메소드
-//	public void sendPushMessage(PushDTO pushDto) {
-//		// 연결된 클라이언트가 없으면 그냥 리턴
-//	    if (clients.size() == 0)	return;
-//
-//	    // 자바 객체를 JSON 문자열로 변환
-//	    ObjectMapper objectMapper = new ObjectMapper();
-//	    String msg;
-//		try {
-//			msg = objectMapper.writeValueAsString(pushDto);
-//		} catch (JsonProcessingException e) {
-//			System.out.println("JSON Error:" + e.getMessage());
-//			return;
-//		}
-//
-//		// FE에 전송할 JSON 메시지객체 생성
-//		TextMessage message = new TextMessage(msg);
-//
-//		// 블럭안에 코드를 수행하는 동안 map 객체에 대한 다른 스레드의 접근을 방지한다.
-//		synchronized (clients) {
-//		    for(WebSocketSession sess: clients) {
-//		    	try {
-//		    		sess.sendMessage(message);
-//		    	} catch (IOException e) {
-//		    		System.out.println(sess.getRemoteAddress() + ":" + e.getMessage());
-//		    	}
-//		    }
-//		}
-//	}
 }
--- PowerForecast/src/main/java/edu/pnu/domain/AlertListener.java
+++ PowerForecast/src/main/java/edu/pnu/domain/AlertListener.java
@@ -1,5 +1,7 @@
 package edu.pnu.domain;
 
+import java.text.SimpleDateFormat;
+
 import edu.pnu.config.AppWebSocketConfig;
 import edu.pnu.config.SpringContext;
 import jakarta.persistence.PostPersist;
@@ -10,13 +12,19 @@ public class AlertListener {
 	public void onPostPersist(Alert alert) {
 		// AppWebSocketConfig 빈을 SpringContext에서 가져옵니다.
 		AppWebSocketConfig webSocketConfig = SpringContext.getBean(AppWebSocketConfig.class);
+		System.out.println(alert.toString());
 
-		System.out.println("Alert added: " + alert.getRegion().getRegionId() + " - " + alert.getAlertType());
-
+		String alertType = alert.getAlertType().equals(AlertType.ABNORMAL) ? "이상" 
+						: alert.getAlertType().equals(AlertType.INCREASE) ? "상승" : "하락";
+			
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 M월 dd일 a hh:mm");
+		String msg = alert.getRegion().getSido() + " " + alert.getRegion().getGugun() + " " 
+					+ alert.getRegion().getEupmyeondong() + " " + sdf.format(alert.getAlertTime())
+					+ " 전력 수요 " + alertType + "이 예상됩니다.";
+		System.out.println(msg);
 		if (webSocketConfig != null) {
-			String message = "이상현상 발생: " + alert.getAlertType() + " in region " + alert.getRegion().getRegionId();
-			System.out.println("Sending message: " + message);
-			webSocketConfig.sendPushMessage(alert.getRegion().getRegionId(), message);
+			System.out.println("Sending message: " + msg);
+			webSocketConfig.sendPushMessage(alert.getRegion().getRegionId(), msg);
 		} else {
 			System.err.println("WebSocketConfig is null");
 		}
